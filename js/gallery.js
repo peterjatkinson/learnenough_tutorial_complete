45fd26e20b096ad189387b3d0bd8b2ef8244317c
Peter Atkinson <peterjatkinson@gmail.com>
2021-01-17 14:56:03 +0000

    End of 11.2

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,20 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+      //alternative way of doing the above line:
+      //let newImageAlt = thumbnail.alt;
+      //mainImage.setAttribute("alt", mainImageAlt);
+      //or could do it by getting the data-title info I suppose, similar to how the largeVersion info is retrieved above
+    });
+  });
 }
