c9505dcf7a15063447f11cde30536c98a78d3e34
Peter Atkinson <peterjatkinson@gmail.com>
2021-01-23 14:37:54 +0000

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -3,18 +3,25 @@
 // and respond appropriately on click.
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo img");
+  let mainImage  = document.querySelector("#gallery-photo img");
+  // Image info to be updated
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
-      // Set clicked image as main image.
+      // Set clicked image as display image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
-      mainImage.setAttribute("alt", thumbnail.alt);
-      //alternative way of doing the above line:
-      //let newImageAlt = thumbnail.alt;
-      //mainImage.setAttribute("alt", mainImageAlt);
-      //or could do it by getting the data-title info I suppose, similar to how the largeVersion info is retrieved above
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
